18c3f7a194ed72f1198717285e39034b0fdaad51
MkTGts <MkTGts>
2024-07-16 22:43:45 +0300

    Добавлен многопользовательский режим

--- main.py
+++ main.py
@@ -6,44 +6,46 @@ from random import randint
 import json
 
 
-bot_token = "token bot"  # сюда нужно установить токен бота
+bot_token = "bot_token"  # сюда нужно установить токен бота
 ATTEMPTS = 5  # количество попыток
 BOT = Bot(bot_token)
 dp = Dispatcher()
 random_dig = lambda: randint(1, 100)  # ф-ция генерации рандомного числа
+users = {}  
 
 
-users = {}
-get_id = requests.get(
-    f"https://api.telegram.org/bot{bot_token}/getUpdates"
-    ).json()
-id = get_id["result"][0]["message"]["from"]["id"]
-if id not in users:
-    users[id] = {
-    "in_game": False,
-    "dig": None,
-    "attemp": None,
-    "tot_game": 0,
-    "win": 0
-}
-user = users[id]
+# функция проверяет есть ли юзер в базе
+def get_user(message: Message):
+    if message.from_user.id not in users:
+        users[message.from_user.id] = {
+            "in_game": False,
+            "dig": None,
+            "attemp": None,
+            "tot_game": 0,
+            "win": 0
+        }
+    return users[message.from_user.id]
 
 
 @dp.message(Command(commands=["start"]))
 async def command_start(message: Message):
+    user = get_user(message)
     await message.answer(text="Это игра угадай число.\nЧто бы прочитать правила введи команду /help\n"
                          "Начнем игру? (ответь Да или Нет)")
 
+        
 
 @dp.message(Command(commands=["help"]))
 async def command_help(message: Message):
+    user = get_user(message)
     await message.answer(text=f"Я загадаю целое цисло от 1 до 100.\nТебе нужно его угадать.\n"
-                         "Дается {ATTEMPTS} попытки.\nЧто бы начать напиши go или нажми команду /go")
+                         f"Дается {ATTEMPTS} попытки.\nЧто бы начать напиши go или нажми команду /go")
 
 
 # команда вывода статистики игрока
 @dp.message(Command(commands=["stat"]))
 async def command_stat(message: Message):
+    user = get_user(message)
     await message.answer("Ваша статистика:\n"
                          f"Всего игр: {user['tot_game']}\n"
                          f"Побед: {user['win']}")
@@ -52,6 +54,7 @@ async def command_stat(message: Message):
 # команда остановки игры
 @dp.message(Command(commands=['cancel']))
 async def command_cancel(message: Message):
+    user = get_user(message)
     if user["in_game"]:
         user["in_game"] = False
         user["dig"] = None
@@ -66,6 +69,7 @@ async def command_cancel(message: Message):
 # хендлер команды для начала игры
 @dp.message(Command(commands=["go"]))
 async def command_go(message: Message):
+    user = get_user(message)
     if not user["in_game"]:
         user["in_game"] = True
         user["dig"] = random_dig()
@@ -81,12 +85,14 @@ async def command_go(message: Message):
 @dp.message(F.text.lower().in_(["да", "lf", "давай", "хочу", "go",
                                                              "играть", "игра", "хочу играть"]))
 async def key_word_for_command_go(message: Message):
+    user = get_user(message)
     await command_go(message)
                 
 
 # хендлер обрабатывающий отказ пользователя
 @dp.message(F.text.lower().in_(["нет", "не", "не хочу", "ytn"]))
 async def procces_negative_answer(message: Message):
+    user = get_user(message)
     if not user["in_game"]:
         await message.answer("Жаль\nЕсли захотите поиграть просто напишите об этом.")
     else:
@@ -98,6 +104,7 @@ async def procces_negative_answer(message: Message):
 # хендлер срабатываюзий при отправке пользователем число по условию и обрабатывающий число в игре
 @dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
 async def procces_answer_digit(message: Message):
+    user = get_user(message)
     if user["in_game"]:
         answer_dig = int(message.text)  # число, которое пользровател отправил
 
@@ -135,6 +142,7 @@ async def procces_answer_digit(message: Message):
 # хэндлер будет срабатывать на любые остальные сообщения
 @dp.message()
 async def procces_other_message(message: Message):
+    user = get_user(message)
     if user["in_game"]:
         await message.answer("Мы же сейчас играем.\n"
                              "Присылайте числа от 1 до 100")
